3128912eceded09feed7313546e0fb26de0bcf42
JeongJunNyeong <JeongJunNyeong@192.168.1.4>
2019-05-25 15:27:25 +0900

    변경

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.print("다시 처음부터... ");
-		System.out.print("천천히 해봐라");
+		System.out.print("");
+		System.out.print("천천히 하자..");
 	}
 }
